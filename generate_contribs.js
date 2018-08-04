4d1428c64a4eed1ac8c7a7995e2be754711a02fc
Montique Stevens <mstevens528@gmail.com>
2018-08-04 02:25:20 -0400

    modified info

--- generate_contribs.js
+++ generate_contribs.js
@@ -6,9 +6,9 @@ const { exec } = require('child_process')
 const execAsync = promisify(exec)
 const writeFileAsync = promisify(writeFile)
 
-const AUTHOR = 'username'
-const EMAIL = 'email'
-const DAYS_TO_GENERATE = 365
+const AUTHOR = 'nycbeardo'
+const EMAIL = 'mstevens528@gmail.com'
+const DAYS_TO_GENERAlTE = 100
 
 const dateMinusGivenDays = days => {
   const d = new Date()
